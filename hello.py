a27f5a42dff9bf85a444f0cf26668dc20c15c449
root <thiagonuness@gmail.com>
2022-05-07 09:29:51 +0200

    Meu primeiro Hello world

---
+++ hello.py
@@ -0,0 +1,7 @@
+#todo programa python e um script python...um conjunto de comandos
+#que sera interpretado pelo python
+#script e um conjunto de comandos que poderiam ser execudados
+#separadamente e coloca encadeado um atras do outro.
+
+print("Hello, World!")
+
